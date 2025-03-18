ad9cbfe056d5f2735002247bc5c9f7b3fef3947f
SikhaChauhan <csikha1010@gmail.com>
2025-03-18 12:04:34 +0530

    [Sikha] Add: initial setup of DbContext is done for implementation purpose

---
+++ PracticeWork/PracticeWork/Controllers/AddressBookController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace PracticeWork.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AddressBookController : ControllerBase
+    {
+        
+    }
+}
---
+++ PracticeWork/RepositoryLayer/Service/AddressBookDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore; 
+using ModelLayer.Model;
+
+namespace RepositoryLayer.Service
+{
+    public class AddressBookDbContext : DbContext 
+    {
+        public AddressBookDbContext(DbContextOptions<AddressBookDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<AddressBookEntity> AddressBooks { get; set; }
+    }
+}
